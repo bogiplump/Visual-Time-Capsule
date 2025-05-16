773f33fe71f9396e5905d7395465a4464b9b0894
bogiplump <120481013+bogiplump@users.noreply.github.com>
2025-05-16 15:47:04 +0300

    Added spring initializr

---
+++ src/main/java/com/java/web/virtual/time/capsule/VirtualTimeCapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.java.web.virtual.time.capsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTimeCapsuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualTimeCapsuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/web/virtual/time/capsule/VirtualTimeCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.web.virtual.time.capsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTimeCapsuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
