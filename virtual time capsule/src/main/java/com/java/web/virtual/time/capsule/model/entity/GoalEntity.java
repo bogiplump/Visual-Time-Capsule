e544484950ea3026d3da356c70e7d5def5b586e8
bogiplump <120481013+bogiplump@users.noreply.github.com>
2025-05-22 18:44:42 +0300

    Added entity classes.

--- virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/model/entity/GoalEntity.java
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/model/entity/GoalEntity.java
@@ -1,17 +1,39 @@
 package com.java.web.virtual.time.capsule.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+
+import java.util.Date;
 
 import lombok.Data;
 
-@Entity
 @Data
+@Entity
 @Table(name = "goals")
 public class GoalEntity {
     @Id
     @GeneratedValue
     private Integer id;
+
+    @Column(name = "is_visible")
+    private boolean isVisible;
+    @Column(name = "is_achieved")
+    private boolean isAchieved;
+    @Column(name = "created_at")
+    private Date creationDate;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by_id")
+    private UserEntity creator;
+
+    @OneToOne(mappedBy = "goal")
+    private CapsuleEntity capsule;
 }
--- virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/repository/VirtualCapsuleRepository.java
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/repository/VirtualCapsuleRepository.java
@@ -1,10 +1,10 @@
 package com.java.web.virtual.time.capsule.repository;
 
-import com.java.web.virtual.time.capsule.model.entity.VirtualCapsuleEntity;
+import com.java.web.virtual.time.capsule.model.entity.CapsuleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VirtualCapsuleRepository extends JpaRepository<VirtualCapsuleEntity, Integer> {
+public interface VirtualCapsuleRepository extends JpaRepository<CapsuleEntity, Integer> {
 }
