e9b966acd05b5c80c298643be67b1c74b48eaee7
Radopeev <95441540+Radopeev@users.noreply.github.com>
2025-05-24 13:58:47 +0300

    Build: add docker

--- virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/VirtualTimeCapsuleApplication.java
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/VirtualTimeCapsuleApplication.java
@@ -1,5 +1,6 @@
 package com.java.web.virtual.time.capsule;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualTimeCapsuleApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.
+			configure().directory("/app").load();
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+
 		SpringApplication.run(VirtualTimeCapsuleApplication.class, args);
 	}
 
