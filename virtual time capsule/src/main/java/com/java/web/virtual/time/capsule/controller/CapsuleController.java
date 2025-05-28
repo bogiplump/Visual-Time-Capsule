763661f4f563120baca230e83da7cb09715442c0
TrayanDKirov <trayandimchev@gmail.com>
2025-05-28 13:50:35 +0300

    Added initial version of the CapsuleController

---
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/controller/CapsuleController.java
@@ -0,0 +1,67 @@
+package com.java.web.virtual.time.capsule.controller;
+
+import com.java.web.virtual.time.capsule.dto.CapsuleCreateDto;
+import com.java.web.virtual.time.capsule.dto.MemoryCreateDto;
+import com.java.web.virtual.time.capsule.model.entity.CapsuleEntity;
+import com.java.web.virtual.time.capsule.service.CapsuleService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/capsule")
+public class CapsuleController {
+    CapsuleService capsuleService;
+
+    public CapsuleController(CapsuleService capsuleService) {
+        this.capsuleService = capsuleService;
+    }
+
+    @PostMapping
+    public void createCapsule(@RequestBody CapsuleCreateDto capsuleDto) {
+        capsuleService.createCapsule(capsuleDto);
+    }
+
+    @GetMapping("/{id}")
+    public CapsuleEntity getCapsuleById(@PathVariable Long id) {
+        return capsuleService.getCapsuleById(id);
+    }
+
+    @GetMapping("/all")
+    public Set<CapsuleEntity> getAllCapsules() {
+        return capsuleService.getAllCapsulesOfUser();
+    }
+
+    @PatchMapping("/{id}/lock")
+    public void lockCapsule(@PathVariable Long id, @RequestParam String openDate) {
+        capsuleService.lockCapsuleById(id, openDate);
+    }
+
+    @PatchMapping("/{id}")
+    public void addNewMemoryToCapsule(@PathVariable Long id, @RequestBody MemoryCreateDto memoryDto) {
+        capsuleService.addMemoryToCapsule(id, memoryDto);
+    }
+
+    @PatchMapping("/{id}")
+    public void addExistingMemoryToCapsule(@PathVariable Long id, @RequestParam Long memoryId) {
+        capsuleService.putMemoryInCapsule(id, memoryId);
+    }
+
+    @PatchMapping("{id}")
+    public void removeMemoryFromCapsule(@PathVariable Long id, @RequestParam Long memoryId) {
+        capsuleService.removeMemoryFromCapsule(id, memoryId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCapsule(@PathVariable Long id) {
+        capsuleService.deleteCapsuleById(id);
+    }
+}
