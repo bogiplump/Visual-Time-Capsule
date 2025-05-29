809499db9b89f2562800f40c8b42c59952664e40
TrayanDKirov <trayandimchev@gmail.com>
2025-05-29 22:47:29 +0300

    Added memory and goal service

---
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/service/GoalService.java
@@ -0,0 +1,17 @@
+package com.java.web.virtual.time.capsule.service;
+
+import com.java.web.virtual.time.capsule.exception.goal.GoalNotFound;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface GoalService {
+    /**
+     * Deletes the goal entity associated with this id from the database.
+     *
+     * @param goalId unique identifier of the goal entity, must not be null.
+     *
+     * @throws IllegalArgumentException if goalId is null.
+     * @throws GoalNotFound if goal with this id does not exist.
+     */
+    void deleteGoal(Long goalId);
+}
---
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/service/MemoryService.java
@@ -0,0 +1,24 @@
+package com.java.web.virtual.time.capsule.service;
+
+import com.java.web.virtual.time.capsule.exception.memory.MemoryNotFound;
+import com.java.web.virtual.time.capsule.model.entity.MemoryEntity;
+
+public interface MemoryService {
+    /**
+     * Retrieves a MemoryEntity from the database by its unique identifier.
+     *
+     * @param id the unique identifier of the memory, must not be null.
+     * @return the MemoryEntity with the specified ID, or null if not found.
+     * @throws IllegalArgumentException if id is null.
+     * @throws MemoryNotFound if a memory with this id does not exist.
+     */
+    MemoryEntity getMemoryById(Long id);
+
+    /**
+     * Saves the memory in the database.
+     *
+     * @param memory must not be null.
+     * @throws IllegalArgumentException if memory is null.
+     */
+    void saveMemory(MemoryEntity memory);
+}
