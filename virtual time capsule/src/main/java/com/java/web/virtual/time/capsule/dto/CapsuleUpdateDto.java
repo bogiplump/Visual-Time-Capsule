fda24f5a2a5554c602a99a07d3cce97a9254fff1
TrayanDKirov <trayandimchev@gmail.com>
2025-05-30 21:12:06 +0300

    Added create and update dtos for CapsuleEntity

--- virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/dto/CapsuleUpdateDto.java
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/dto/CapsuleUpdateDto.java
@@ -1,14 +1,20 @@
 package com.java.web.virtual.time.capsule.dto;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
-public class CapsuleCreateDto {
+public class CapsuleUpdateDto {
     @NotBlank(message = "Capsule name must not be blank")
     @Size(max = 20, message = "Capsule name must not exceed 20 characters")
     private String capsuleName;
+
+    @Future(message = "Open date must be in the future")
+    private LocalDateTime openDate;
 
     private GoalDto goal;
 }
