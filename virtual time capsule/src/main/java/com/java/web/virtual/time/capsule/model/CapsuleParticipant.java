385747fa7dce79b5d6722fa4674831adb4ae5847
TrayanDKirov <trayandimchev@gmail.com>
2025-06-09 17:42:52 +0300

    Added CapsuleParticipant entity and repository outline

---
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/model/CapsuleParticipant.java
@@ -0,0 +1,44 @@
+package com.java.web.virtual.time.capsule.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "capsule_participants")
+public class CapsuleParticipant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "capsule_id", nullable = false)
+    private Long capsuleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "participant_id", nullable = false)
+    private Long participantId;
+
+    @Column(name = "ready_to_close", nullable = false)
+    private Boolean isReadyToClose;
+
+    @Column(name = "added_at", nullable = false)
+    @Setter(AccessLevel.NONE)
+    private LocalDateTime addedAt;
+}
---
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/repository/CapsuleParticipantRepository.java
@@ -0,0 +1,7 @@
+package com.java.web.virtual.time.capsule.repository;
+
+import com.java.web.virtual.time.capsule.model.CapsuleParticipant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapsuleParticipantRepository extends JpaRepository<CapsuleParticipant, Long> {
+}
