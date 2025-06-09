61ce644621426120c7572b8fc7d1162379604dd3
TrayanDKirov <trayandimchev@gmail.com>
2025-06-09 20:34:22 +0300

    Finished SharedCapsuleController methods

--- virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/controller/SharedCapsuleController.java
+++ virtual time capsule/src/main/java/com/java/web/virtual/time/capsule/controller/SharedCapsuleController.java
@@ -1,40 +1,65 @@
 package com.java.web.virtual.time.capsule.controller;
 
+import com.java.web.virtual.time.capsule.dto.SharedCapsuleCreateDto;
+import com.java.web.virtual.time.capsule.dto.SharedCapsuleResponseDto;
+import com.java.web.virtual.time.capsule.service.SharedCapsuleService;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.security.Principal;
+
 @RestController
 @RequestMapping("api/v1/shared-capsules")
 public class SharedCapsuleController {
-    @PostMapping
-    public void createSharedCapsule() {
+    private SharedCapsuleService sharedCapsuleService;
 
+    @PostMapping
+    public ResponseEntity<?> createSharedCapsule(@NotNull @RequestBody SharedCapsuleCreateDto sharedCapsuleDto,
+                                                 Principal principal) {
+        Long capsuleId = sharedCapsuleService.createSharedCapsule(sharedCapsuleDto, principal.getName());
+
+        URI location = URI.create("/api/v1/shared-capsules/" + capsuleId);
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{id}")
-    public void getSharedCapsuleById(@NotNull @PathVariable Long id) {
-
+    public ResponseEntity<SharedCapsuleResponseDto> getSharedCapsuleById(@NotNull @PathVariable Long id,
+                                                                         Principal principal) {
+        return ResponseEntity.ok(sharedCapsuleService.getSharedCapsuleById(id, principal.getName()));
     }
 
     @PatchMapping("/{id}")
-    public void addUserToCapsule(@NotNull @PathVariable Long capsuleId, @NotNull @RequestParam Long userId) {
+    public ResponseEntity<?> addUserToCapsule(@NotNull @PathVariable Long capsuleId, @NotNull @RequestParam Long userId,
+                                 Principal principal) {
+        sharedCapsuleService.addUserToCapsule(capsuleId, userId, principal.getName());
 
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}")
-    public void removeUserFromCapsule(@NotNull @PathVariable Long capsuleId, @NotNull @RequestParam Long userId) {
+    public ResponseEntity<?> removeUserFromCapsule(@NotNull @PathVariable Long capsuleId, @NotNull @RequestParam Long userId,
+                                                        Principal principal) {
+        sharedCapsuleService.addUserToCapsule(capsuleId, userId, principal.getName());
 
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteSharedCapsuleById(@NotNull @PathVariable Long id) {
+    public ResponseEntity<?> deleteSharedCapsuleById(@NotNull @PathVariable Long id,
+                                                  Principal principal) {
+        sharedCapsuleService.deleteSharedCapsuleById(id, principal.getName());
 
+        return ResponseEntity.noContent().build();
     }
 }
